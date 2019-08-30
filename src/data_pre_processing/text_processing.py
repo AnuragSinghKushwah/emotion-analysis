12fa8273476e62057e0459755bf0138b2eb6a006
Anurag Singh Kushwah <anurag786kushwah@gmail.com>
2019-08-30 15:49:24 +0530

    completed Prediction for Logistic Regression and SVC

--- src/data_pre_processing/text_processing.py
+++ src/data_pre_processing/text_processing.py
@@ -2,10 +2,12 @@ import pandas as pd
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 
 def remove_stop_words(input_str):
     # Function to remove stop words from input string
@@ -43,37 +45,78 @@ def preprocess_text(csv_file, processed_file):
 
     data_frame = pd.read_csv(csv_file, usecols=["clean_text","emotion"])
 
-    data_frame['clean_text'] = data_frame['clean_text'].fillna("NONE")
+    data_frame = data_frame[data_frame['clean_text'].notnull()]
+    print("data_frame : ",len(data_frame))
 
+    # data_frame['clean_text'] = data_frame['clean_text'].fillna("NONE")
     #Removing Stop words from the clean text
     data_frame['clean_text'] = data_frame['clean_text'].apply(lambda x : remove_stop_words(x))
-    data_frame['clean_text'] = data_frame['clean_text'].fillna("NONE")
+    # data_frame['clean_text'] = data_frame['clean_text'].fillna("NONE")
 
     # Applying Stemmer
     data_frame['clean_text'] = data_frame['clean_text'].apply(lambda x : get_stemmed_text(x))
-    data_frame['clean_text']  = data_frame['clean_text'].fillna("NONE")
+    # data_frame['clean_text']  = data_frame['clean_text'].fillna("NONE")
 
     # Applying Lemmatizer
     data_frame['clean_text'] = data_frame['clean_text'].apply(lambda x : get_lemmatized_text(x))
+    # data_frame['clean_text'] = data_frame['clean_text'].fillna("NONE")
 
-    print(data_frame.head())
+    # Filtering none values
+    filtered_df = data_frame[data_frame['clean_text'].notnull()]
+    # filtered_df = filtered_df[filtered_df['clean_text']!="NONE"]
+
+    print("filtered_df : ",len(filtered_df))
+    print(filtered_df.head())
     data_frame.to_csv(processed_file)
 
 def prepare_train_test_data(processed_file):
     "Function to vectorize input data"
     data_frame = pd.read_csv(processed_file)
     print("data_frame : ",data_frame.head())
+    data_frame = data_frame[data_frame['clean_text'].notnull() | data_frame['clean_text'].notna()] #df[df['name'].notnull() | df['foo'].notnull()]
     targets = data_frame['emotion'].values
     tweets = data_frame['clean_text'].values
-    print(targets[:10])
-    print(tweets[:10])
+    print("total_tweets : ",len(targets))
+    print("total_targets: ",len(tweets))
+
+    tweets_train_clean = tweets[:round(len(tweets)*0.7)]
+    targets_train_clean = targets[:round(len(targets)*0.7)]
+    tweets_test_clean = tweets[round(len(tweets)*0.7):]
+    targets_test_clean = targets[round(len(targets)*0.7):]
+
+    print("tweets_train_clean  : ",len(tweets_train_clean))
+    print("targets_train_clean : ",len(targets_train_clean))
+    print("tweets_test_clean   : ",len(tweets_test_clean))
+    print("targets_test_clean  : ",len(targets_test_clean))
+
     # # Create feature vectors
-    # vectorizer = TfidfVectorizer(min_df=5,
-    #                              max_df=0.8,
-    #                              sublinear_tf=True,
-    #                              use_idf=True)
-    
     ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 3))
+    ngram_vectorizer.fit(tweets_train_clean)
+    X = ngram_vectorizer.transform(tweets_train_clean)
+    X_test = ngram_vectorizer.transform(tweets_test_clean)
+
+    X_train, X_val, y_train, y_val = train_test_split(X, targets_train_clean, train_size=0.75)
+
+    for c in [0.01, 0.05, 0.25, 0.5, 1]:
+        lr = LogisticRegression(C=c)
+        lr.fit(X_train, y_train)
+        print("Logistic Regression : Accuracy for C=%s: %s"% (c, accuracy_score(y_val, lr.predict(X_val))))
+
+    final_ngram = LogisticRegression(C=0.5)
+    final_ngram.fit(X, targets_train_clean)
+    print("Logistic Regression : Final Accuracy: %s"
+          % accuracy_score(targets_test_clean, final_ngram.predict(X_test)))
+
+    print('---------------------------------------------------------')
+
+    for c in [0.01, 0.05, 0.25, 0.5, 1]:
+        svm = LinearSVC(C=c)
+        svm.fit(X_train, y_train)
+        print("SVC : Accuracy for C=%s: %s"% (c, accuracy_score(y_val, svm.predict(X_val))))
+
+    final_svm_ngram = LinearSVC(C=0.01)
+    final_svm_ngram.fit(X, targets_train_clean)
+    print("SVC : Final Accuracy: %s"% accuracy_score(targets_test_clean, final_svm_ngram.predict(X_test)))
 
 
 if __name__ == '__main__':
@@ -82,3 +125,4 @@ if __name__ == '__main__':
     processed_file = "C:\\Users\\Anurag\\PycharmProjects\\DeepLearning\\emotion-analysis\\dataset\\processed_tweets.csv"
     # preprocess_text(csv_file=csv_file,processed_file=processed_file)
     prepare_train_test_data(processed_file=processed_file)
+#
